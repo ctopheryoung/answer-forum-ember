ae36184e750b6c40bdb44863aca2668a8e4d6296
Chris Young <ctopheryoung@gmail.com>
2016-04-01 10:28:24 -0700

    implement question-display on index page

---
+++ app/models/question.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  user: DS.attr(),
+  date: DS.attr(),
+  question: DS.attr(),
+  note: DS.attr(),
+  answers: DS.hasMany('answer', {async: true})
+});
--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'answer-forum',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://answer-forum.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
