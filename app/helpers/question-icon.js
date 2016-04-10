95fa63c0765f3bc56a149226a5c341bc014ec084
Chris Young <ctopheryoung@gmail.com>
2016-04-10 15:05:14 -0700

    add upvote and downvote icons

--- app/helpers/question-icon.js
+++ app/helpers/question-icon.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export function questionIcon(params) {
   var question = params[0];
-
+  console.log(question.title);
   if(question.get('answers').get('length') === 0) {
-    return Ember.String.htmlSafe('<p>Needs an answer!</p>');
+    return Ember.String.htmlSafe('<span class="noAnswerFlag">Answer now!</span>');
   }
 }
 
